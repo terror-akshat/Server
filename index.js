24754f1fb18621960ed68f38a9214928de6098f1
Akshat <www.akshatagarwal123klj@gmail.com>
2025-02-19 00:06:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = socketIo(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://funny-trifle-39cd51.netlify.app",
         methods: ["GET", "POST"],
     },
 });
